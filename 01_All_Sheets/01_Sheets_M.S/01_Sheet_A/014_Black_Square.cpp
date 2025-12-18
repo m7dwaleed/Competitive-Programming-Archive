591932e3156f257c3bd9c9378682542668f18963
Mohamed Waleed <m7dwaleed@gmail.com>
2025-12-18 19:28:45 +0200

    Codeforces - Black_Square

--- 01_All_Sheets/01_Sheets_M.S/01_Sheet_A/014_Black_Square.cpp
+++ 01_All_Sheets/01_Sheets_M.S/01_Sheet_A/014_Black_Square.cpp
@@ -1,16 +1,40 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+int main()
+{
 
+    int arr[4];
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> arr[i];
+    }
 
-int main(){
+    string s;
+    cin >> s;
 
-int arr[4];
-for(int =0;i<n;i++){
-    cin>>arr[i];
-}
+    int sum = 0;
 
+    for (int i = 0; i < s.length(); i++)
+    {
+        switch (s[i])
+        {
+        case '1':
+            sum += arr[0];
+            break;
+        case '2':
+            sum += arr[1];
+            break;
+        case '3':
+            sum += arr[2];
+            break;
+        case '4':
+            sum += arr[3];
+        }
 
+       
+    }
 
+    cout << sum;
     return 0;
 }
